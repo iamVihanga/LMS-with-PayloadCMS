9e3e699c939195e518c3c6088b6f3fd0f32e3d16
iamVihanga <vihangarashansilva@gmail.com>
2025-11-14 00:27:40 +0530

    feat: add PostCSS configuration and integrate Tailwind CSS; refactor HomePage layout and styles
    - Created a new PostCSS configuration file to enable Tailwind CSS.

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,11 +1,9 @@
 import { headers as getHeaders } from 'next/headers.js'
-import Image from 'next/image'
 import { getPayload } from 'payload'
 import React from 'react'
 import { fileURLToPath } from 'url'
 
 import config from '@/payload.config'
-import './styles.css'
 
 export default async function HomePage() {
   const headers = await getHeaders()
@@ -13,23 +11,14 @@ export default async function HomePage() {
   const payload = await getPayload({ config: payloadConfig })
   const { user } = await payload.auth({ headers })
 
-  const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
-
   return (
-    <div className="home">
-      <div className="content">
-        <picture>
-          <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg" />
-          <Image
-            alt="Payload Logo"
-            height={65}
-            src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg"
-            width={65}
-          />
-        </picture>
-        {!user && <h1>Welcome to your new project.</h1>}
-        {user && <h1>Welcome back, {user.email}</h1>}
-        <div className="links">
+    <div className="bg-zinc-900 h-screen w-full flex items-center justify-center">
+      <div className="space-y-3">
+        {!user && (
+          <h1 className="font-semibold text-lg text-white">Welcome to your new project.</h1>
+        )}
+        {user && <h1 className="text-white/80">Welcome back, {user.email}</h1>}
+        <div className="flex items-center gap-3 text-white/80">
           <a
             className="admin"
             href={payloadConfig.routes.admin}
@@ -47,12 +36,6 @@ export default async function HomePage() {
             Documentation
           </a>
         </div>
-      </div>
-      <div className="footer">
-        <p>Update this page by editing</p>
-        <a className="codeLink" href={fileURL}>
-          <code>app/(frontend)/page.tsx</code>
-        </a>
       </div>
     </div>
   )
